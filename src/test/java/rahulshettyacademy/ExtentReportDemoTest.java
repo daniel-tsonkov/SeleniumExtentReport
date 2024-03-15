a7a6d641060685b71aa49623463e278341c707f1
dan <daniel_tzonkov@abv.bg>
2024-03-15 10:48:58 +0200

    15032024

--- src/test/java/rahulshettyacademy/ExtentReportDemoTest.java
+++ src/test/java/rahulshettyacademy/ExtentReportDemoTest.java
@@ -1,5 +1,7 @@
 package rahulshettyacademy;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,21 +11,33 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class ExtentReportDemoTest {
+    ExtentReports extent;
+
     @BeforeTest
     public void config() {
         //we nee from two classes. They are very helpful
         //ExtentReports and ExtentSparkReporter
-        String path = System.getProperty("user.dir") + "\\reports\\index.htmo";
+        String path = System.getProperty("user.dir") + "\\reports\\index.html";
         ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Web Automation Results");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "My name should be here :)");
     }
+
     @Test
     public void initialDemo() {
+        ExtentTest test = extent.createTest("Initial Demo");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/client/");
+        driver.get("https://rahulshettyacademy.com/");
         System.out.println("Title: " + driver.getTitle());
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.close();
+        test.fail("Result doesn't match");
 
-
+        extent.flush();
     }
 }

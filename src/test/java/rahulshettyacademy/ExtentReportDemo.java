c95d773e3e24cadd4b647a4427f50d43bab53ff4
dan <daniel_tzonkov@abv.bg>
2024-03-15 09:57:34 +0200

    15032024

--- src/test/java/rahulshettyacademy/ExtentReportDemo.java
+++ src/test/java/rahulshettyacademy/ExtentReportDemo.java
@@ -1,5 +1,20 @@
 package rahulshettyacademy;
 
-public class ExtentReportDemo {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+import java.time.Duration;
+
+public class ExtentReportDemo {
+    @Test
+    public void initialDemo() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/client/");
+        System.out.println("Title: " + driver.getTitle());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+    }
 }

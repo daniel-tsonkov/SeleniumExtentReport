8489f190d7e6b10cb2091598c3154ac4daf18fa2
dan <daniel_tzonkov@abv.bg>
2024-03-15 10:08:19 +0200

    15032024

--- src/test/java/rahulshettyacademy/ExtentReportDemoTest.java
+++ src/test/java/rahulshettyacademy/ExtentReportDemoTest.java
@@ -1,12 +1,21 @@
 package rahulshettyacademy;
 
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class ExtentReportDemo {
+public class ExtentReportDemoTest {
+    @BeforeTest
+    public void config() {
+        //we nee from two classes. They are very helpful
+        //ExtentReports and ExtentSparkReporter
+        String path = System.getProperty("user.dir") + "\\reports\\index.htmo";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+    }
     @Test
     public void initialDemo() {
         WebDriver driver = new ChromeDriver();
